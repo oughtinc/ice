abb04fa163072132c366138126de137d755151d0
Sophia Xu <i@sophiaxu.me>
2022-10-10 16:46:46 +0000

    implement interactive prompt editor

--- ui/components/TracePage/PromptEditor.tsx
+++ ui/components/TracePage/PromptEditor.tsx
@@ -17,6 +17,44 @@ const PromptEditorContext = createContext<{
   closeEditor: () => void;
 } | null>(null);
 
+function saveCaretPosition(context: any, plusOne?: boolean){
+  var sel = window.getSelection();
+  if (!sel || sel.rangeCount < 1) return;
+  var selection = sel;
+  var range = selection.getRangeAt(0);
+  if (!range) return;
+  range.setStart(context, 0);
+  var len = range.toString().length;
+
+  return function restore(){
+    try {
+      var pos = getTextNodeAtPosition(context, len);
+      selection.removeAllRanges();
+      var range = new Range();
+      range.setStart(pos.node ,pos.position + (plusOne ? 1 : 0));
+      selection.addRange(range);
+    } catch (e) {
+      // pass
+    }
+  }
+}
+
+function getTextNodeAtPosition(root: any, index: any){
+  const NODE_TYPE = NodeFilter.SHOW_TEXT;
+  var treeWalker = document.createTreeWalker(root, NODE_TYPE, function next(elem: any) {
+      if(index > elem?.textContent?.length){
+          index -= elem?.textContent?.length;
+          return NodeFilter.FILTER_REJECT
+      }
+      return NodeFilter.FILTER_ACCEPT;
+  });
+  var c = treeWalker.nextNode();
+  return {
+      node: c? c: root,
+      position: index
+  };
+}
+
 export const PromptEditorProvider = ({ children }: { children: ReactNode }) => {
   const [open, setOpen] = useState(false);
   const [currentPrompt, setCurrentPrompt] = useState("");
@@ -52,6 +90,9 @@ export const usePromptEditorContext = () => {
 export const PromptEditorModal = () => {
   const { open, currentPrompt, setCurrentPrompt, closeEditor } = usePromptEditorContext();
   const promptBoxRef = useRef<HTMLDivElement | null>(null);
+  const promptContentRef = useRef<HTMLDivElement | null>(null);
+
+  const [promptResult, setPromptResult] = useState('');
 
   useEffect(() => {
     if (!open) return () => false;
@@ -65,6 +106,28 @@ export const PromptEditorModal = () => {
     document.addEventListener("keyup", closeOnEscape);
     return () => document.removeEventListener("keyup", closeOnEscape);
   }, [open, closeEditor]);
+
+  useEffect(() => {
+    if (!promptContentRef.current || !promptResult.length) return;
+    const span = document.createElement('span');
+    span.id = 'promptResult';
+    span.style.backgroundColor = 'skyblue';
+    span.appendChild(document.createTextNode(promptResult));
+    const oldSpan = document.getElementById('promptResult');
+    if (!oldSpan) promptContentRef.current.appendChild(span);
+    else promptContentRef.current.replaceChild(span, oldSpan);
+  }, [promptResult]);
+
+  const handleInput = useCallback((ev: any) => {
+    setCurrentPrompt((ev.target as HTMLSpanElement).innerText);
+  }, [setCurrentPrompt]);
+
+  useEffect(() => {
+    if (!promptContentRef.current) return;
+    const restore = saveCaretPosition(promptContentRef.current, currentPrompt.endsWith('\n'));
+    promptContentRef.current.textContent = currentPrompt;
+    if (restore) restore();
+  }, [currentPrompt])
 
   return (
     <div
@@ -84,12 +147,11 @@ export const PromptEditorModal = () => {
         <div className="flex flex-col">
           <h3 className="text-lg font-semibold text-gray-700 mb-2">Prompt</h3>
           <div
-            className="rounded-4 border-slate-200 border flex-grow px-3 py-2 whitespace-pre-wrap"
+            className="rounded-4 border-slate-200 border flex-grow px-3 py-2 whitespace-pre-wrap inline-block"
             contentEditable
-            onChange={ev => setCurrentPrompt((ev.target as HTMLSpanElement).innerText)}
-          >
-            {currentPrompt}
-          </div>
+            onInput={ev => handleInput(ev)}
+            ref={promptContentRef}
+          />
         </div>
         <div className="flex flex-col w-48">
           <h3 className="text-lg font-semibold text-gray-700 mb-2">Agent</h3>
@@ -108,6 +170,10 @@ export const PromptEditorModal = () => {
               className="rounded-4 px-2 py-1 h-fit !shadow-none hover:bg-slate-200 mt-4 text-xs"
               size="md"
               variant="outline"
+              onClick={() => {
+                setTimeout(() => setPromptResult('The quick brown fox'), 500);
+                setTimeout(() => setPromptResult('The quick brown fox jumps over the lazy dog.'), 1000);
+              }}
             >
               Prompt model
             </Button>
