76cd84b815010efe83b22e75a6f9fcfd44b78d21
orowa <orowa@thenetworklab.co>
2022-10-12 00:56:07 +0100

    Equivalency between primer recipe and custom implementation

--- debate.py
+++ debate.py
@@ -2,17 +2,16 @@ from ice.agents.base import Agent
 from ice.recipe import recipe
 from ice.recipes.primer.debate.prompt import *
 
-async def debate(question: str = "Should we ban guns?", agent_names: tuple[Name, Name] = ("Alice", "Bob"), number_of_turns: int = 10):
+async def debate(question: str = "Should we ban guns?", number_of_turns: int = 8, agent_names: tuple[Name, Name] = ("Alice", "Bob")):
+    agents = [recipe.agent(), recipe.agent()]
     debate_history = initialize_debate(question, agent_names)
-    for i in range(number_of_turns):
-        if i % 2 == 0:
-            speaker, listener = agent_names
-        else:
-            listener, speaker = agent_names
-        prompt = render_debate_prompt(speaker, debate_history, number_of_turns-i)
-        answer = (await recipe.agent().answer(prompt=prompt)).strip('" ')
-        answer = answer.split(listener)[0].strip()
-        debate_history.append((speaker, answer))
+    turns_left = number_of_turns
+    while turns_left > 0:
+        for agent, name in zip(agents, agent_names):
+            prompt = render_debate_prompt(name, debate_history, turns_left)
+            answer = await agent.answer(prompt=prompt, multiline=False)
+            debate_history.append((name, answer.strip('" ')))
+            turns_left -= 1
     return render_debate(debate_history)
 
 recipe.main(debate)
