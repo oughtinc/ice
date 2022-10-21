831466eb01ff2ff38e86144f9ff307dc590c850e
CG40899 <cg80492@gmail.com>
2022-10-21 11:46:19 +0000

    Make Ought inference key None by default.

--- ice/settings.py
+++ ice/settings.py
@@ -11,13 +11,14 @@ else:
 
 
 class Settings(BaseSettings):
-    OUGHT_INFERENCE_API_KEY: str
+    OUGHT_INFERENCE_API_KEY: str | None = None
     OPENAI_API_KEY: str
     OPENAI_ORG_ID: str
     OUGHT_INFERENCE_URL: AnyHttpUrl = "https://inference.elicit.org"
     GOLD_STANDARDS_CSV_PATH: str = "gold_standards/gold_standards.csv"
     GS_QUOTE_FOUND_THRESHOLD: float = 0.75
     CACHE_DIR: Path = Path(__file__).parent.parent / "cache/"
+    ELICIT_AUTH_TOKEN: str = ""
 
     class Config:
         env_file = ".env"
