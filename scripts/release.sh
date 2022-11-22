fc2250a0b629e76853cab0441e6a4b4274ba9ab1
Alex Hall <alex.mojaki@gmail.com>
2022-11-22 21:55:34 +0200

    review comments

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
-set -eux
 
-# Ensure that there are no uncommitted changes
-# which would mess up using the git tag as a version
-[ -z "$(git status --porcelain)" ]
+set -eux -o pipefail
 
-if [ -z "${1+x}" ]
-then
+if [ ! -z "$(git status --porcelain)" ]; then
+    set +x
+    echo You have uncommitted changes which would mess up the git tag
+    exit 1
+fi
+
+if [ $(git rev-parse --abbrev-ref HEAD) != "main" ]; then
+    set +x
+    echo You must be on the main branch to release
+    exit 1
+fi
+
+if [ -z "${1+x}" ]; then
     set +x
     echo Provide a version argument
     echo "${0} <major>.<minor>.<patch>"
     exit 1
-else
-    if [[ ${1} =~ ^([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
-        :
-    else
-        echo "Not a valid release tag."
-        exit 1
-    fi
+fi
+
+if [[ ! ${1} =~ ^([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
+    echo "Not a valid release tag."
+    exit 1
 fi
 
 npm --prefix ui run build
