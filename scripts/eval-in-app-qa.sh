38bcdf8d93da08c6a246444786c2e31f510aa03b
Luke Stebbing <luke@ought.org>
2022-11-23 09:42:40 -0500

    Keep running the Elicit script inside of Docker

--- scripts/eval-in-app-qa.sh
+++ scripts/eval-in-app-qa.sh
@@ -9,7 +9,7 @@ mkdir -p ${ICE_DIR}/data/in_app_qa_results
 cd ${ELICIT_NEXT_DIR}
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 docker compose -f docker-compose.yaml -f docker-compose.$os.yaml up -d
-LOG_LEVEL=warn TOKENIZERS_PARALLELISM=false poetry run python -m api.eval.main > ${ICE_DIR}/data/in_app_qa_results/qa_eval_output.csv
+docker compose exec api bash -c 'LOG_LEVEL=warn TOKENIZERS_PARALLELISM=false poetry run python -m api.eval.main' > ${ICE_DIR}/data/in_app_qa_results/qa_eval_output.csv
 
 cd ${ICE_DIR}
 python -m ice.evaluation.summarize_experiment_evals data/in_app_qa_results/qa_eval_output.csv
