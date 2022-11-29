0e116ece8799026dc890c6d7f1128f1b72e420c2
Luke Stebbing <luke@ought.org>
2022-11-29 13:54:42 -0500

    Listen on all interfaces so localhost urls always work

--- ice/server.py
+++ ice/server.py
@@ -2,6 +2,7 @@ from uvicorn import main
 
 params_by_name = {p.name: p for p in main.params}
 params_by_name["app"].default = "ice.routes.app:app"
+params_by_name["host"].default = "0.0.0.0"
 params_by_name["port"].default = 8935
 
 if __name__ == "__main__":
