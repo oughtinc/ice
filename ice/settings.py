dfbd1bde24eb88d2d7643f13063e19a19b60adad
Luke Stebbing <luke@ought.org>
2022-12-01 17:19:13 -0500

    Absolutify gold standards csv path

--- ice/settings.py
+++ ice/settings.py
@@ -16,7 +16,9 @@ class Settings(BaseSettings):
     OPENAI_ORG_ID: str = ""
     OUGHT_INFERENCE_API_KEY: str = ""
     OUGHT_INFERENCE_URL: AnyHttpUrl = "https://prod.elicit.org"
-    GOLD_STANDARDS_CSV_PATH: str = "gold_standards/gold_standards.csv"
+    GOLD_STANDARDS_CSV_PATH: Path = (
+        Path(__file__).parent.parent / "gold_standards/gold_standards.csv"
+    )
     GS_QUOTE_FOUND_THRESHOLD: float = 0.75
 
 
