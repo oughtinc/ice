c8f7eb8f09fea8c799bde20478d3b8d0bf57f481
Luke Stebbing <luke@ought.org>
2022-12-07 11:26:58 -0500

    Drop expandable

--- ui/components/TracePage/CallIconButton.tsx
+++ ui/components/TracePage/CallIconButton.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@chakra-ui/react";
 import { CaretDown, CaretRight, ChatCenteredDots } from "phosphor-react";
 
-export function CallIconButton({
+export const CallIconButton = ({
   expanded,
   onChange,
   childCount,
@@ -11,23 +11,20 @@ export function CallIconButton({
   onChange: (expanded: boolean) => void;
   childCount: number;
   isModelCall: boolean;
-}) {
-  const expandable = !isModelCall && childCount > 0;
-  return (
-    <Button
-      className="rounded-full p-1 h-fit mr-2 !shadow-none hover:bg-slate-200 w-12"
-      isActive={expanded}
-      {...(!expandable
-        ? {}
-        : {
-            "aria-label": expanded ? "Collapse" : "Expand",
-            leftIcon: expanded ? <CaretDown /> : <CaretRight />,
-            onClick: () => onChange(expanded),
-          })}
-      size="md"
-      variant="outline"
-    >
-      <span className="mr-1">{isModelCall ? <ChatCenteredDots /> : childCount || "𝑓"}</span>
-    </Button>
-  );
-}
+}) => (
+  <Button
+    className="rounded-full p-1 h-fit mr-2 !shadow-none hover:bg-slate-200 w-12"
+    isActive={expanded}
+    {...(isModelCall || !childCount
+      ? {}
+      : {
+          "aria-label": expanded ? "Collapse" : "Expand",
+          leftIcon: expanded ? <CaretDown /> : <CaretRight />,
+          onClick: () => onChange(expanded),
+        })}
+    size="md"
+    variant="outline"
+  >
+    <span className="mr-1">{isModelCall ? <ChatCenteredDots /> : childCount || "𝑓"}</span>
+  </Button>
+);
