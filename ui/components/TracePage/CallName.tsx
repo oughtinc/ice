5b647e4297b8ccff04e8567ee1c6f208ad6b475c
Alex Hall <alex.mojaki@gmail.com>
2022-12-07 22:53:58 +0200

    Simplify getFormattedName using lodash, rearrange, inline CallNameProps

--- ui/components/TracePage/CallName.tsx
+++ ui/components/TracePage/CallName.tsx
@@ -1,6 +1,17 @@
 import classNames from "classnames";
+import { upperFirst } from "lodash";
 
-export const CallName = ({ className, name, cls }: CallNameProps) => {
+export const getFormattedName = (snakeCasedName: string) => {
+  const spacedName = snakeCasedName.replace(/_/g, " ");
+  return upperFirst(spacedName);
+};
+
+export interface CallFunction {
+  name: string; // function name
+  cls?: string; // class name for methods
+}
+
+export const CallName = ({ className, name, cls }: CallFunction & { className?: string }) => {
   const displayName = (name === "execute" || name === "run") && cls ? cls : name;
   return (
     <div className="flex items-center gap-1">
@@ -10,21 +21,4 @@ export const CallName = ({ className, name, cls }: CallNameProps) => {
       <span className={className}>{getFormattedName(displayName)}</span>
     </div>
   );
-};
-
-export interface CallFunction {
-  name: string; // function name
-  cls?: string; // class name for methods
-}
-
-interface CallNameProps extends CallFunction {
-  className?: string;
-}
-
-export const getFormattedName = (snakeCasedName: string) => {
-  const spacedName = snakeCasedName.replace(/_/g, " ");
-  const capitalizedAndSpacedName = spacedName
-    ? spacedName[0].toUpperCase() + spacedName.slice(1)
-    : snakeCasedName;
-  return capitalizedAndSpacedName;
 };
