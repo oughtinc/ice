ac47d6bd7efff2016e70af8bf2c66b7f39a22c40
Alex Hall <alex.mojaki@gmail.com>
2022-12-22 10:43:08 +0200

    remove commented code

--- ui/components/TracePage/FString.tsx
+++ ui/components/TracePage/FString.tsx
@@ -33,27 +33,8 @@ export const FString = ({ parts }: { parts: FlattenedFStringPart[] }) => {
         oddValue = !oddValue;
         const inner = part.formatted;
 
-        // This commented code allows seeing the original nested construction,
-        // if the value isn't flattened.
-        // const inner =
-        //   typeof part.value === "object" && "__fstring__" in part.value ? (
-        //     <FString parts={part.value.__fstring__ as FStringPart[]} />
-        //   ) : (
-        //     part.formatted
-        //   );
-
         return (
-          <span
-            key={i}
-            title={part.source}
-            className={`p2 bg-${color}-50 text-${color}-600`}
-            // style={{
-            //   border: "1px solid #ccc",
-            //   borderRadius: "4px",
-            //   padding: "1px 2px",
-            //   display: "inline-block",
-            // }}
-          >
+          <span key={i} title={part.source} className={`p2 bg-${color}-50 text-${color}-600`}>
             {inner}
           </span>
         );
