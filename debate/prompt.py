4201775a6f2fbfbf89faabaea7214d53f5c776e4
Yumibyte <ashraigosa@gmail.com>
2023-01-08 11:36:32 -0800

    class setup for multiple prompts. Refactor of variable names

--- debate/prompt.py
+++ debate/prompt.py
@@ -1,9 +1,18 @@
-from ice.recipes.primer.debate.utils import *
+from utils import *
+import testDebates
 
 def render_debate_prompt(agent_name: str, debate: Debate, turns_left: int) -> str:
+
+    # retrieve testDebates class
+    test_debates_instance = testDebates.TestDebates.get_instance()
+    # retrieve correct prompt
+    prompts_list = test_debates_instance.get_prompts()
+    promptIn = prompts_list[current]
+
     prompt = f"""
-    "You are {agent_name}. There are {turns_left} turns left in the debate. You are trying to win the debate using reason and evidence. Don't repeat yourself. No more than 1-2 sentences per turn.
+    "You are {agent_name}. There are {turns_left} turns left in the debate. {promptIn}
     {render_debate(debate, agent_name)}
     You: "
     """.strip()
     return prompt
+print(render_debate_prompt("Bob", my_debate, 5))
--- debate/testDebates.py
+++ debate/testDebates.py
@@ -1,16 +1,59 @@
 import os
 
-listOfQuestions = [
-"Is a hotdog a taco?",
-"Should we legalize all drugs?",
-"Is water wet?"
-]
-# promptIn = "You are trying to win the debate using reason and evidence. No more than 1-2 sentences per turn."
+class TestDebates:
 
-# we need a bash prompt that will
-# execute recipe file with the above inputs
+    _instance= None
 
-for question in listOfQuestions:
-    print("question!" + question)
-    command = f"python debate/recipe.py --question \"{question}\" "
-    os.system(command)
+    def __init__(self):
+
+        self.current_prompt = 0
+
+        self.prompts_list = ["You are trying to win the debate using reason and evidence. Don't repeat yourself. No more than 1-2 sentences per turn.",
+        "You are trying to win the debate using reason and evidence. No more than 1-2 sentences per turn."]
+
+        self.questions_list = ["Is a hotdog a taco?"]
+
+    def get_current_prompt(self):
+        return self.current_prompt
+    def get_prompts(self):
+
+        return self.listOfPrompts
+
+    def get_questions(self):
+
+        return self.listOfQuestions
+        # "Should we legalize all drugs?",
+        # "Is water wet?"
+
+    # promptIn = "You are trying to win the debate using reason and evidence. No more than 1-2 sentences per turn."
+
+    # we need a bash prompt that will
+    # execute recipe file with the above inputs
+
+    def execute_debate(self):
+
+        questions_list = self.get_questions()
+
+        prompts_list = self.get_prompts()
+
+        for prompt in listOfPrompts:
+
+            # increment our current prompt
+            self.current_prompt = self.current_prompt + 1
+
+            for question in questions_list:
+                command = f"python debate/recipe.py --question \"{question}\" "
+                os.system(command)
+    @staticmethod
+    def get_instance():
+        if(TestDebates._instance == None):
+            TestDebates._instance = TestDebates()
+            print("new instance of testDebates")
+        return TestDebates._instance
+# Initialize our class of prompts/questions
+if __name__ == '__main__':
+
+    testDebates = TestDebates.get_instance()
+
+    # immediately start the program based on the prompts/questions
+    testDebates.execute_debate()
