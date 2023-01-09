f1b10aaabe5afbbabfcbe952fbe4536cb99f1617
Yumibyte <ashraigosa@gmail.com>
2023-01-08 21:39:54 -0800

    modify class to utilize json and fix bugs with loading JSON. Need to add functionality to reset counter

--- debate/prompt.py
+++ debate/prompt.py
@@ -3,16 +3,15 @@ import testDebates
 
 def render_debate_prompt(agent_name: str, debate: Debate, turns_left: int, current_prompt: int) -> str:
 
-    test_debates_instance = testDebates.TestDebates()
+    test_debates_instance = testDebates.TestDebates("debate/inputs.json")
     # retrieve correct prompt
-    prompts_list = test_debates_instance.get_prompts()
-
-    print("CURRENT NUM: " + str(current_prompt))
-    print("CURRENT: " + str(test_debates_instance.get_prompts()))
-    promptIn = prompts_list[current_prompt]
+    current_prompt = test_debates_instance.get_current_prompt()
+    #
+    # print("CURRENT NUM: " + str(current_prompt))
+    # print("CURRENT: " + str(test_debates_instance.get_prompts()))
 
     prompt = f"""
-    "You are {agent_name}. There are {turns_left} turns left in the debate. {promptIn}
+    "You are {agent_name}. There are {turns_left} turns left in the debate. {current_prompt}
     {render_debate(debate, agent_name)}
     You: "
     """.strip()
--- debate/testDebates.py
+++ debate/testDebates.py
@@ -1,31 +1,37 @@
 import os
+import json
 
 class TestDebates:
 
     def __init__(self, file_name):
         JSON_loaded = open(file_name)
         self.file_name = file_name
-        self.input_data = json.loads(JSON_loaded)
+        self.input_data = json.load(JSON_loaded)
 
-    def reset_prompt_count(self, file_name):
+    def reset_prompt_count(self):
 
-        self.input_data["count"] = 0
-        with open(file_name, "w") as outfile:
+        self.input_data["prompt_count"] = 0
+        with open(self.file_name, "w") as outfile:
             json.dump(self.input_data, outfile)
-    # getters
-    def get_current_prompt(self):
-        return self.input_data
-
-    def increment_current_prompt(self):
-        self.current_prompt = self.current_prompt + 1
 
     def get_prompts(self):
+        return self.input_data["input_prompts"]
 
-        return self.prompts_list
+    # getters
+    def get_current_prompt(self):
+        prompts_list = self.get_prompts()
+        prompt_count = self.input_data["prompt_count"]
+        return prompts_list[prompt_count]
+
+    def increment_current_prompt(self):
+        prompt_count = self.input_data["prompt_count"]
+        self.input_data["prompt_count"] = prompt_count + 1
+        with open(self.file_name, "w") as outfile:
+            json.dump(self.input_data, outfile)
 
     def get_questions(self):
 
-        return self.questions_list
+        return self.input_data["input_questions"]
         # "Should we legalize all drugs?",
         # "Is water wet?"
 
@@ -51,7 +57,7 @@ class TestDebates:
 if __name__ == '__main__':
 
     # initialize instance
-    test_debates_instance = TestDebates()
+    test_debates_instance = TestDebates("debate/inputs.json")
 
     # immediately start the program based on the prompts/questions
     test_debates_instance.execute_debate()
