0f63dfe035e12be9f958466f6913fbf2a9a59466
Luke Stebbing <luke@ought.org>
2023-01-10 14:24:03 -0500

    Fix pagination

--- ui/components/TracePage/Table.tsx
+++ ui/components/TracePage/Table.tsx
@@ -2,6 +2,7 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { uniq } from "lodash";
+import { Fragment } from "react";
 
 export type StringToScalar = Record<string, boolean | number | string | null>;
 
@@ -13,15 +14,21 @@ export const Table = ({
   rows: StringToScalar[];
   rowIds?: string[];
   onFocusChange?: (rowId?: string) => void;
-}) => (
-  <AgGridReact<StringToScalar>
-    className="ag-theme-alpine w-full h-full"
-    columnDefs={uniq(rows.flatMap(Object.keys)).map(field => ({ field }))}
-    defaultColDef={{ filter: true, sortable: true, resizable: true }}
-    onCellFocused={({ rowIndex }) =>
-      onFocusChange?.(rowIndex === null ? undefined : rowIds?.[rowIndex])
-    }
-    pagination={rows.length > 100}
-    rowData={rows}
-  ></AgGridReact>
-);
+}) => {
+  const pagination = rows.length > 100;
+  return (
+    // AgGridReact doesn't respect changes to 'pagination' while the component is mounted.
+    <Fragment key={String(pagination)}>
+      <AgGridReact<StringToScalar>
+        className="ag-theme-alpine w-full h-full"
+        columnDefs={uniq(rows.flatMap(Object.keys)).map(field => ({ field }))}
+        defaultColDef={{ filter: true, sortable: true, resizable: true }}
+        onCellFocused={({ rowIndex }) =>
+          onFocusChange?.(rowIndex === null ? undefined : rowIds?.[rowIndex])
+        }
+        pagination={pagination}
+        rowData={rows}
+      ></AgGridReact>
+    </Fragment>
+  );
+};
