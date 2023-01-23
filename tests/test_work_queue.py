ba6caa7c43bef23128809f3717784c75bdd22134
Jess Smith <smith.jessk@gmail.com>
2023-01-23 13:16:21 -0500

    Clean up a tad

--- tests/test_work_queue.py
+++ tests/test_work_queue.py
@@ -1,4 +1,7 @@
 import asyncio
+import random
+
+from typing import List
 
 
 class WorkQueue:
@@ -7,8 +10,9 @@ class WorkQueue:
     def __init__(self, max_concurrency: int):
         self.max_concurrency = max_concurrency
         self.queue: asyncio.Queue = asyncio.Queue()  # TODO why not use a regular queue?
+        self.workers: List[asyncio.Task] = []
 
-    async def put(self, task: asyncio.Task):
+    async def do(self, task: asyncio.Task):
         """returns when the task is done"""
         cv = asyncio.Condition()
         await self.queue.put((task, cv))
@@ -19,41 +23,51 @@ class WorkQueue:
 
     def start(self):
         for _ in range(self.max_concurrency):
-            asyncio.create_task(self._work())
+            self.workers.append(asyncio.create_task(self._work()))
+
+    async def stop(self):
+        # TODO is this really 'force stop'?
+        for worker in self.workers:
+            worker.cancel()
 
     async def _work(self):
         while True:
             task, cv = await self.queue.get()
-            await asyncio.create_task(self.run_task(task))
+            await task
             async with cv:
                 cv.notify_all()
 
-    async def run_task(self, task: asyncio.Task):
-        print("running")
-        await task
-        print("done with task")
+
+MAX_CONCURRENCY = 2
+MAX_TIME_TO_SLEEP = 3
+CONSTANT_RESULT = 1
 
 
 def test_work_queue():
     async def task():
-        await asyncio.sleep(0.1)
-        return 1
+        time_to_sleep = random.random() * MAX_TIME_TO_SLEEP
+        print(f"sleeping for {time_to_sleep}")
+        await asyncio.sleep(time_to_sleep)
+        return CONSTANT_RESULT
 
     async def run():
-        queue = WorkQueue(max_concurrency=2)
+        queue = WorkQueue(max_concurrency=MAX_CONCURRENCY)
         queue.start()
         enqueued = []
-        for _ in range(3):
-            print("putting")
-            # TODO make these tasks run in parallel
+        for _ in range(MAX_CONCURRENCY * 2):
             t = asyncio.create_task(task())
-            enqueued += [queue.put(t)]  # it's not really "put", it's "run"
-        all = await asyncio.gather(*enqueued)
-        print(all)
-        assert all == [1, 1, 1]
+            enqueued += [queue.do(t)]
+        results = []
+        for x in asyncio.as_completed(enqueued):
+            results += [await x]
+            print(f"got result {results[-1]}")
+        assert results == [CONSTANT_RESULT] * (MAX_CONCURRENCY * 2)
+        await queue.stop()
 
     asyncio.run(run())
 
 
 if __name__ == "__main__":
+    # TODO take this out? it helped me :)
+    # TODO why does collecting (in pytest) take so long? (it doesn't always... not sure what's up)
     test_work_queue()
