0355c6c83a96686cabb4b91cbc870a837675ee21
Jess Smith <smith.jessk@gmail.com>
2023-01-23 13:57:28 -0500

    actually enforce concurrency controls through thunks

--- tests/test_work_queue.py
+++ tests/test_work_queue.py
@@ -1,7 +1,11 @@
 import asyncio
-import random
+import uuid
 
+from collections.abc import Callable
+from typing import Any
+from typing import Dict
 from typing import List
+from typing import TypeVar
 
 
 class WorkQueue:
@@ -11,15 +15,23 @@ class WorkQueue:
         self.max_concurrency = max_concurrency
         self.queue: asyncio.Queue = asyncio.Queue()  # TODO why not use a regular queue?
         self.workers: List[asyncio.Task] = []
+        self.results: Dict[uuid.UUID, Any] = {}
 
-    async def do(self, task: asyncio.Task):
+    T = TypeVar("T")
+
+    async def do(self, f: Callable[[T], asyncio.Task], arg: T):
         """returns when the task is done"""
+        import uuid
+
+        u = uuid.uuid4()  # TODO assert u not in self.results
         cv = asyncio.Condition()
-        await self.queue.put((task, cv))
+        await self.queue.put((u, cv, f, arg))
         async with cv:
             await cv.wait()
+        result = self.results[u]  # TODO dunder?
+        del self.results[u]
         # TODO maybe better error handling here
-        return task.result()
+        return result
 
     def start(self):
         for _ in range(self.max_concurrency):
@@ -32,8 +44,11 @@ class WorkQueue:
 
     async def _work(self):
         while True:
-            task, cv = await self.queue.get()
-            await task
+            uuid, cv, f, arg = await self.queue.get()
+            print(f"invoking f with {arg}")
+            task = f(arg)
+            result = await task
+            self.results[uuid] = result
             async with cv:
                 cv.notify_all()
 
@@ -43,25 +58,29 @@ MAX_TIME_TO_SLEEP = 3
 CONSTANT_RESULT = 1
 
 
-def test_work_queue():
-    async def task():
-        time_to_sleep = random.random() * MAX_TIME_TO_SLEEP
-        print(f"sleeping for {time_to_sleep}")
-        await asyncio.sleep(time_to_sleep)
-        return CONSTANT_RESULT
+async def task(time_to_sleep: float):
+    return await asyncio.sleep(time_to_sleep, result=time_to_sleep)
 
+
+def f(time_to_sleep: float) -> asyncio.Task:
+    t = asyncio.create_task(task(time_to_sleep))
+    t.set_name(f"task {time_to_sleep}")
+    return t
+
+
+def test_work_queue():
     async def run():
         queue = WorkQueue(max_concurrency=MAX_CONCURRENCY)
         queue.start()
         enqueued = []
-        for _ in range(MAX_CONCURRENCY * 2):
-            t = asyncio.create_task(task())
-            enqueued += [queue.do(t)]
+        times = range(10 + MAX_CONCURRENCY * 2, 0, -1)
+        for time_to_sleep in times:
+            enqueued += [queue.do(f, time_to_sleep)]
         results = []
         for x in asyncio.as_completed(enqueued):
             results += [await x]
             print(f"got result {results[-1]}")
-        assert results == [CONSTANT_RESULT] * (MAX_CONCURRENCY * 2)
+        assert set(results) == set(times)
         await queue.stop()
 
     asyncio.run(run())
