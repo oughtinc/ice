5663c6a372267f630c650532cfde9dd244a73c8e
Brian Muhia <iambrianmuhia@gmail.com>
2023-09-02 21:21:47 +0300

    Edit subquestions

--- amplification.py
+++ amplification.py
@@ -1,5 +1,5 @@
 from ice.recipe import recipe
-from ice.recipes.primer.subquestions import ask_subquestions
+from subquestions import ask_subquestions
 from ice.utils import map_async
 from functools import partial
 from fvalues import F
@@ -63,7 +63,7 @@ Answer: "
 async def get_subs(
     question: str = "What is the effect of creatine on cognition?", engine="chatgpt"
 ):
-    subquestions = await ask_subquestions(question=question)
+    subquestions = await ask_subquestions(question=question, engine=engine)
     subs_answer_fn = partial(sub_answer, question=question, engine=engine)
     subanswers = await map_async(subquestions, subs_answer_fn)
     return list(zip(subquestions, subanswers))
---
+++ subquestions.py
@@ -0,0 +1,25 @@
+from fvalues import F
+
+from ice.recipe import recipe
+
+
+def make_subquestion_prompt(question: str) -> str:
+    return F(
+        f"""Decompose the following question into 2-5 subquestions that would help you answer the question. Make the questions stand alone, so that they can be answered without the context of the original question.
+
+Question: "{question}"
+Subquestions:
+-"""
+    ).strip()
+
+
+async def ask_subquestions(
+    question: str = "What is the effect of creatine on cognition?", engine: str = "gpt-4"
+):
+    prompt = make_subquestion_prompt(question)
+    subquestions_text = await recipe.agent(agent_name=engine).complete(prompt=prompt)
+    subquestions = [line.strip("- ") for line in subquestions_text.split("\n")]
+    return subquestions
+
+
+recipe.main(ask_subquestions)
